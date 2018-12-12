ec03868680a5defd097000c9f35ff79bc095d3b2
Andy Aylward <37965077+andyaylward@users.noreply.github.com>
2018-12-12 09:35:26 -0700

    fix pageload

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ function pageLoadBodyEffect() {
     document.querySelector("body").style.opacity = 1;
     document.querySelector("body").style.transform = "translate(0,0)";
 }
-// window.onload = pageLoadBodyEffect;
-
-window.addEventListener('load', pageLoadBodyEffect);
+window.onload = pageLoadBodyEffect;
 
 // About Image
 const aboutImgList = ["https://images.unsplash.com/photo-1509114397022-ed747cca3f65?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2daae6fa88d0422996cb571d9e41681e&auto=format&fit=crop&w=675&q=80", "https://images.unsplash.com/photo-1542979666-0d943dd97b2f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7cbc560177a228210b6cae2fcb166233&auto=format&fit=crop&w=1267&q=80", "https://images.unsplash.com/photo-1542690969-8b36d56ca4e4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=9e37508e6b0a3dd9b27b113e2e4b9231&auto=format&fit=crop&w=1350&q=80"]
