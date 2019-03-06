6c3a6056948a13721a875ffceb2220b5672957f9
Andy Aylward <37965077+andyaylward@users.noreply.github.com>
2019-03-06 16:01:12 -0700

    Added Drupal to skills. Added 2 Projects. Cleaned some JS code.

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ setInterval(function(){
 },4000)
 
 // Project Gallery
+// GIFS, HEADINGS, DESCRIPTIONS, LINKS
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
 
@@ -34,47 +35,99 @@ const projectLink = document.getElementById('projectLink');
 const projectLink2 = document.getElementById('projectLink2');
 
 const projects = [
+  // RESPONSIVE PAGE
     'response.gif',
+  // FIN GALLERY
+    'fin.gif',
+  // FIRST CHOICE NON-MED
+    'firstchoice.gif',
+  // PET PROJECT ANIMAL ADOPTION
     'animal.gif', 
+  // CANADA QUIZ
     'quiz.gif', 
+  // CALGARY SNOWSPORTS
     'snow.gif', 
+  // KAIZEN AUTOMOTIVE
     'kaizen.gif', 
+  // MINDWELL-U
     'MWU.gif', 
+  // BREAKING BAD QUOTES
     'quotes.gif', 
+  // INSTAGRAM PAGE
     'insta.gif',
+  // LYRIC GENERATOR
     'lyrics.gif'
 ]; 
 const heading = [
+  // RESPONSIVE PAGE
     'Responsively Changing Page',
+  // FIN GALLERY
+    'Fin Gallery',
+  // FIRST CHOICE NON-MED
+    'First Choice Non-Medical Home Care',
+  // PET PROJECT ANIMAL ADOPTION
     'Animal Adoption Page', 
+  // CANADA QUIZ 
     'Canada Quiz', 
+  // CALGARY SNOWSPORTS
     'Fictional Winter Sporting Good Business', 
+  // KAIZEN AUTOMOTIVE   
     'Kaizen Automotive Landing Page Re-Design', 
+  // MINDWELL-U    
     'MindWell-U Landing Re-Design', 
+  // BREAKING BAD QUOTES 
     'Breaking Bad Quote Generator', 
+  // INSTAGRAM PAGE
     'Instagram Desktop Replica Using PHP', 
+  // LYRIC GENERATOR
     'Song Lyric Generator'
 ];
 const description = [
+  // RESPONSIVE PAGE
     'A site that completely changes at different width resizes',
+  // FIN GALLERY
+    'A site made for a local aquatic pet store using WordPress, done during an internship with Simple Simple Advertising',
+  // FIRST CHOICE NON-MED
+    'A website designed and devleoped by me. Coded from scratch using HTML, CSS, and Javascipt. Done for a local client who owns a personal home care business',
+  // PET PROJECT ANIMAL ADOPTION
     'A responsive site made about a fictional animal adoption company.',
+  // CANADA QUIZ 
     'A 5 question triva quiz about Canada. Gives you a percentage score at the end.', 
+  // CALGARY SNOWSPORTS
     'A site that was made using WordPress and WooCommerce plug-in. Based on a fictional local business that sells snowboard and ski equipment', 
+  // KAIZEN AUTOMOTIVE    
     'A landing page re-design of a Calgary company called Kaizen Automotive. Modified car colors in Photoshop. Able to choose the cars color using JavaScript loop',
+  // MINDWELL-U
     'Responsive website re-design for a Vancouver based company.',
+  // BREAKING BAD QUOTES 
     'A page that cycles through several Breaking Bad quotes.',
+  // INSTAGRAM PAGE 
     'A remake of the desktop version of Instagram. HTML and CSS was coded from scratch, PHP was coded with Laravel framework. Each user generated with migrations and has 5 posts. Users can also register an account and select custom handle and avatar', 
+  // LYRIC GENERATOR
     'Can type in a song and artist to return the lyrics of the requested song. Song must be in Lyrics.ovh API in order to be succesfull'
 ];
 const link = [
+  // RESPONSIVE PAGE
     'https://andysresponsivepagedesign-crzukxapxa.now.sh/#',
+  // FIN GALLERY
+    'https://fin-gallery.ca/',
+  // FIRST CHOICE NON-MED
+    'https://fcnmhctest-kknonzwmxf.now.sh',
+  // PET PROJECT ANIMAL ADOPTION
     'https://andy-aylward-cpnt-assignment-4-fpswjrnnoq.now.sh', 
+  // CANADA QUIZ
     'https://canada-quiz-page-zvigvenqga.now.sh', 
+  // CALGARY SNOWSPORTS
     'http://35.247.63.134/',
+  // KAIZEN AUTOMOTIVE 
     'https://kaizen-automotive-wvulowodqa.now.sh/', 
+  // MINDWELL-U
     'https://andyaylward-mindwelluexercise2-htondefrpz.now.sh', 
+  // BREAKING BAD QUOTES  
     'https://breaking-bad-quote-machine-rifdvzhegq.now.sh', 
+  // INSTAGRAM PAGE  
     'http://52.87.237.138/', 
+  // LYRIC GENERATOR 
     'https://lyric-finder-izoozjfhzo.now.sh/'
 ];
 
@@ -119,7 +172,7 @@ const htmlLogo = document.getElementById('htmlLogo');
 const psLogo = document.getElementById('psLogo');
 const cssLogo = document.getElementById('cssLogo');
 const jsLogo = document.getElementById('jsLogo');
-const jqLogo = document.getElementById('jqLogo');
+const drupalLogo = document.getElementById('drupalLogo');
 const phpLogo = document.getElementById('phpLogo');
 const bsLogo = document.getElementById('bsLogo');
 const techContent = document.getElementById('techContent');
@@ -146,7 +199,7 @@ jsLogo.addEventListener('click', function(){
     techContent.style.opacity = '0';
 
     setTimeout(function(){
-        techContent.innerHTML = 'JavaScript';
+        techContent.innerHTML = 'JavaScript + jQuery';
         techContent.style.opacity = '1';
     }, 200)
 })
@@ -160,11 +213,11 @@ wpLogo.addEventListener('click', function(){
     }, 200)
 })
 
-jqLogo.addEventListener('click', function(){
+drupalLogo.addEventListener('click', function(){
     techContent.style.opacity = '0';
 
     setTimeout(function(){
-        techContent.innerHTML = 'jQuery';
+        techContent.innerHTML = 'Drupal 7 / 8';
         techContent.style.opacity = '1';
     }, 200)
 })
@@ -173,7 +226,7 @@ cssLogo.addEventListener('click', function(){
     techContent.style.opacity = '0';
 
     setTimeout(function(){
-        techContent.innerHTML = 'CSS 3';
+        techContent.innerHTML = 'CSS 3 + SASS';
         techContent.style.opacity = '1';
     }, 200)
 })
